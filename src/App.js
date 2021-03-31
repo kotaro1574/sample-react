68f331564174df9b73a6eb5e4ecf575c7666ea57
kotaro1574 <shake1574@gmail.com>
2021-03-31 12:17:31 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { createStore } from 'redux';
 
-function App() {
-  let items = [
-    { "name": "いちご", "price": "100" },
-    { "name": "りんご", "price": "150" },
-    { "name": "バナナ", "price": "230" },
-  ]
+const App = () => (
+  <BrowserRouter>
+    <div class="container text-center mt-5">
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/blog/:id" component={Blog} />
+      <Route path="/sum/:num1/:num2" component={Sum} />
+    </div>
+  </BrowserRouter>
+)
+
+const vote = (state = 0, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1
+    default:
+      return state
+  }
+}
+
+let store = createStore(vote)
+
+const Home = () => {
+  store.dispatch({ type: 'ADD' })
+  store.dispatch({ type: 'ADD' })
+  let x = store.getState().toString()
 
   return (
-    <div className="App container">
-      <table className="table table-striped">
-        <tbody>
-          {items.map((value) => (
-            <tr>
-              <th scope="row">{ value.name }</th>
-              <td>{ value.price }円</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <h1>Welcome</h1>
+
+      <p>投票数: {x}</p>
+      <p><Link to="/about">About</Link></p>
+    </div>
+  )
+}
+
+const About = () => {
+  store.dispatch({ type: 'ADD' })
+  let x = store.getState().toString();
+  return (
+    <div>
+      <h1>About</h1>
+      <p>投票数: {x}</p>
+    </div>
+  )
+}
+
+const Blog = props => {
+  const { id } = props.match.params
+  
+  return (
+    <div>
+      <p>{id}番目</p>
+    </div>
+  )
+}
+
+const Sum = props => {
+  const { num1, num2 } = props.match.params
+  
+  return (
+    <div>
+      <p>{num1}+{num2} = {parseInt(num1)+parseInt(num2)}</p>
     </div>
   )
 }
